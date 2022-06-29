6bbf7d9c51e95c111f67f25fc845dde0445e6d89
DNA601 <dnavarrete6001@gmail.com>
2022-06-28 18:49:47 -0700

    timer

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const startQuizButton = document.getElementById('startQuiz-btn')
+const startQuizButton = document.getElementById('startQuiz-btn');
 
-const questionContainerElement = document.getElementById('questions-container')
+const questionContainerElement = document.getElementById('questions-container');
 
 startQuizButton.addEventListener('click', startGame)
     //lets the user click on the start quiz button
@@ -8,5 +8,25 @@ startQuizButton.addEventListener('click', startGame)
 function startGame() {
     console.log('Started')
     startQuizButton.classList.add('hide')
-    questionContainerElement.classList.remove('hide')
+    questionContainerElement.classList.remove('hide');
+
+    var timeEl = document.querySelector(".time");
+    var mainEl = document.getElementById("head");
+
+    var secondsLeft = 60;
+
+    function setTime() {
+        var timerInterval = setInterval(function() {
+            secondsLeft--;
+            timeEl.textContent = secondsLeft + " seconds until your quiz ends!";
+            if (secondsLeft === 0) {
+                clearInterval(timerInterval)
+                sendMessage();
+            }
+        }, 1000)
+    }
+    setTime();
+    //sets timer after quiz starts
+
+
 }
